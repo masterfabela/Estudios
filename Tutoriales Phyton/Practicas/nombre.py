f7af226a74c00b1feff22d41b74568064f93df97
José Ramón Vieites <jose.vieites@ALTIA.VIGO>
2017-08-18 13:17:58 +0200

    Arreglado nombre

--- Tutoriales Phyton/Practicas/nombre.py
+++ Tutoriales Phyton/Practicas/nombre.py
@@ -1,10 +1,4 @@
 
-# Este programa pregunta tu nombre
-print '¿Cómo te llamas?'
-tuNombre = raw_input()
-print 'Encantado de conocerte, ' + tuNombre
-
-
 #Este programa pregunta o teu nome
 print('Como te chamas?')
 nome = raw_input()

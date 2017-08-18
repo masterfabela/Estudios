88da52b70504512b6e677d98ea18f1bca4df9c84
José Ramón Vieites <jose.vieites@ALTIA.VIGO>
2017-08-18 12:54:51 +0200

    Practicas de Phyton añadidas

---
+++ Tutoriales Phyton/Practicas/nombre.py
@@ -0,0 +1,11 @@
+
+# Este programa pregunta tu nombre
+print '¿Cómo te llamas?'
+tuNombre = raw_input()
+print 'Encantado de conocerte, ' + tuNombre
+
+
+#Este programa pregunta o teu nome
+print('Como te chamas?')
+nome = raw_input()
+print('Encantado de coñecerte '+nome)

bc35921f18142fe87171fc994e385fb5b48d3dca
femio23 <chiscazo.paco@gmail.com>
2017-10-18 09:35:33 +0200

    Adición de exemplo de instanciado de obxetos e metodo.

--- FRMCode/NetBeansProjects/Exemplo_Métodos/src/exemplo_métodos/Exemplo_Métodos.java
+++ FRMCode/NetBeansProjects/Exemplo_Métodos/src/exemplo_métodos/Exemplo_Métodos.java
@@ -1,8 +1,24 @@
 package exemplo_métodos;
-
+import java.util.Scanner;
 public class Exemplo_Métodos {
 
     public static void main(String[] args) {
+        //creacion de obxeto sen valores
+        Rectangulo primeiro=new Rectangulo();
+        primeiro.amosar();
+        //creacion do obxeto con valores
+        Rectangulo segundo=new Rectangulo(3,5);
+        segundo.amosar();
+        //creacion de obxeto con valores introducidos por teclado
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Introduza a base:");
+        float b=sc.nextFloat();
+        System.out.println("Introduza a altura");
+        float a=sc.nextFloat();
+        Rectangulo terceiro=new Rectangulo(b,a);
+        terceiro.amosar();
+        terceiro.calcArea(b, a);
+        terceiro.calcPerim(b, a);
         
     }
     
--- FRMCode/NetBeansProjects/Exemplo_Métodos/src/exemplo_métodos/Rectangulo.java
+++ FRMCode/NetBeansProjects/Exemplo_Métodos/src/exemplo_métodos/Rectangulo.java
@@ -3,9 +3,12 @@ package exemplo_métodos;
 public class Rectangulo {
     private float base,altura;
     //constructores
+    // 1º:Predeterminado de java
     public Rectangulo(){
     
 }
+    
+    // 2º Instanciado personal.
     public Rectangulo(float ba,float alt){
         base=ba;
         altura=alt;
@@ -26,7 +29,17 @@ return altura;
 }
 
 public void amosar(){
-    System.out.println("A base é de "+getBase+"uds.\n "
-            + "e a altura é de "+getAltura+"uds.");
+    System.out.println("A base é de "+getBase()+"uds.\n "
+            + "e a altura é de "+getAltura()+"uds.");
 } 
+public void calcArea(float ba,float hi ){
+System.out.println("A área do rectangulo de base"+ba+" e altura "+hi
++" é de "+(ba*hi)+"unidades ó cuadrado");
+}
+public float calcPerim(float ba,float hi){
+    float perim=(2*ba+2*hi);
+    System.out.println("O perimetro do rectangulo de base"+ba+" e altura "+hi
++" é de "+perim+"unidades ó cuadrado");
+    return perim;
+}
 }

b3cf6e5a6fb6b661d5d852118556922cd8026dc5
unknown <chiscazo.paco@gmail.com>
2017-10-24 18:07:58 +0200

    Boletin 3_2.

--- FRMCode/NetBeansProjects/Boletin3_2/src/boletin3_2/Boletin3_2.java
+++ FRMCode/NetBeansProjects/Boletin3_2/src/boletin3_2/Boletin3_2.java
@@ -1,21 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package boletin3_2;
-
-/**
- *
- * @author femio23
- */
+import java.utils.Scanner;
 public class Boletin3_2 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+                System.out.println("Vai a lanzar un novo satelite,introduza os seguintes datos:");
+                System.out.println("Meridiano:");
+                double m=sc.nextDouble();
+                System.out.println("Paralelo:");
+                double p=sc.nextDouble();
+                System.out.println("Distacia:");
+                double d=sc.nextDouble();
+                Satelite meteosat=new Satelite(m,p,d);
+                meteosat.verPosicion();    }
     }
     
 }
--- FRMCode/NetBeansProjects/Boletin3_2/src/boletin3_2/Satelite.java
+++ FRMCode/NetBeansProjects/Boletin3_2/src/boletin3_2/Satelite.java
@@ -1,14 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package boletin3_2;
 
-/**
- *
- * @author femio23
- */
 public class Satelite {
-    
+    private double meridiano ;
+    private double paralelo;
+    private double distanciaTerra ;
+ 
+    public Satelite ( ) {
+
+      meridiano = paralelo = distanciaTerra = 0 ;
+    }
+ 
+    public Satelite ( double m, double p , double d ){
+
+        meridiano = m ;
+        paralelo = p;
+        distanciaTerra = d ;
+
+    }
+    public void verPosicion ( ) {
+        System.out.println ("o satelite atopase  no paralelo "+paralelo+", meridiano "+
+        meridiano+" a unha distancia da terra "+ distanciaTerra );
+    }
 }

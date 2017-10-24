312a625121084cb8017fc6655d1bd5e73009451a
unknown <chiscazo.paco@gmail.com>
2017-10-24 18:04:49 +0200

    Boletin 3_1.

--- FRMCode/NetBeansProjects/Boletin3_1/src/boletin3_1/Boletin3_1.java
+++ FRMCode/NetBeansProjects/Boletin3_1/src/boletin3_1/Boletin3_1.java
@@ -4,18 +4,24 @@
  * and open the template in the editor.
  */
 package boletin3_1;
-
-/**
- *
- * @author femio23
- */
+import java.utils.Scanner;
 public class Boletin3_1 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc=new Scanner(System.in);
+        Coche car=new Coche();
+        System.out.println("A velocidade actual do coche é de "+car.getVelocidade()+" Km/h.");
+        System.out.println("Vai vostede a aceleralo coche, diga a velocidade que quere que alcance");
+        int n= sc.nextInt();
+        car.acelerar(n);
+        System.out.println("A velocidade actual do coche é de "+car.getVelocidade()+" Km/h.");
+        System.out.println("Vai vostede a frenalo coche, diga a velocidade que quere que alcance");
+        n= sc.nextInt();
+        car.frenar(n);
+        System.out.println("A velocidade actual do coche é de "+car.getVelocidade()+" Km/h.");
     }
     
 }
--- FRMCode/NetBeansProjects/Boletin3_1/src/boletin3_1/Coche.java
+++ FRMCode/NetBeansProjects/Boletin3_1/src/boletin3_1/Coche.java
@@ -1,14 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package boletin3_1;
 
-/**
- *
- * @author femio23
- */
 public class Coche {
+    private int velocidade ;
     
+    public Coche(){
+       velocidade =0 ;
+    }
+    public int getVelocidade(){
+    return velocidade;
+    }
+    public void acelerar (int valor){
+    velocidade=velocidade+valor;
+    }
+    public void frenar (int menos){
+    velocidade=velocidade-menos;
+ }    
 }

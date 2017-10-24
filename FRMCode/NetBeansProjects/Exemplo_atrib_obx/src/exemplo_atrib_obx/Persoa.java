014b9c61d1ae8eeb63002e3baf4b55a4e832264e
Femio23 <chiscazo.paco@gmail.com>
2017-10-24 10:06:08 +0200

    Exemplo artibutos de obxetos.

---
+++ FRMCode/NetBeansProjects/Exemplo_atrib_obx/src/exemplo_atrib_obx/Persoa.java
@@ -0,0 +1,28 @@
+
+package exemplo_atrib_obx;
+
+public class Persoa {
+ String nome;
+ String dni;
+ public Persoa(){
+ };
+ 
+ public Persoa(String n, String d){
+     nome=n;
+     dni=d;
+ };
+ public void setnome(String n){
+     nome=n;
+ }
+ public void setdni(String d){
+     dni=d;
+ }
+ public String getnome(){  
+     return nome;
+ }
+ 
+ public String getdni(){
+     return dni;
+ }
+ 
+}

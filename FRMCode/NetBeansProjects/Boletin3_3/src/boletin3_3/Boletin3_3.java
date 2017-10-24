d27eb721c73b20578619eacb2c0bc698fc5950ce
unknown <chiscazo.paco@gmail.com>
2017-10-24 18:10:10 +0200

    Boletin3_3.

--- FRMCode/NetBeansProjects/Boletin3_3/src/boletin3_3/Boletin3_3.java
+++ FRMCode/NetBeansProjects/Boletin3_3/src/boletin3_3/Boletin3_3.java
@@ -1,21 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package boletin3_3;
-
-/**
- *
- * @author femio23
- */
+import java.utils.Scanner;
 public class Boletin3_3 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+          System.out.println("Vamos a facer calculos con circulos, polotanto, introduza "
+                  + "os datos para inicializalo");
+          System.out.println("Radio: ");
+          double r=sc.nextDouble();
+          Circulo cir1=new Circulo(r);
+          
+          System.out.println("Para modificalo radio, introduza o seu novo valor");
+          r=sc.nextDouble();
+          cir1.setCirculo(r);
+          cir1.getCirculo();
+          System.out.println("A area do circulo é de "+cir1.calcarea());
+            System.out.println("A circunferencia é de "+cir1.calccir());
     }
     
 }
--- FRMCode/NetBeansProjects/Boletin3_3/src/boletin3_3/Circulo.java
+++ FRMCode/NetBeansProjects/Boletin3_3/src/boletin3_3/Circulo.java
@@ -1,14 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package boletin3_3;
 
-/**
- *
- * @author femio23
- */
 public class Circulo {
+    double radio;
+    static double pi=3.14d;
     
+    public Circulo(){
+    radio=0;
+    }
+    public Circulo(double r){
+    radio=r;
+    }
+    public double getCirculo(){
+        return radio;
+    }
+    public void setCirculo(double r){
+        radio=r;
+    }
+    public double calcarea(){
+    return (pi*Math.pow(radio, radio));
+    }
+    public double calccir(){
+    return (pi*radio*2);
 }

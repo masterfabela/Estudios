d75ac3cbb9f27c87de5e8cdda8a12081eef6bbe8
Femio23 <chiscazo.paco@gmail.com>
2018-01-18 12:32:28 +0100

    Recuperacion de exemploarraybasicos.

--- FRMCode/NetBeansProjects/ExemploArraisObxetos/src/exemploarraisobxetos/ExemploArraisObxetos.java
+++ FRMCode/NetBeansProjects/ExemploArraisObxetos/src/exemploarraisobxetos/ExemploArraisObxetos.java
@@ -6,9 +6,11 @@ public class ExemploArraisObxetos {
 
     public static void main(String[] args) {
         MetodosArray ma1=new MetodosArray();
-        ma1.amosar(ma1.creararray());
+        Persoa []pa1=ma1.creararray();
+        ma1.amosar(pa1);
         String dni=JOptionPane.showInputDialog("Que DNI buscas?");
-        ma1.buscar(ma1.creararray(),dni);
+        ma1.buscar(pa1,dni);
+        System.out.println(ma1.buscar(pa1,dni));
         
     }
     

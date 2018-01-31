16d2dc35a02f2069b1549c2b608d6eec0dddafe2
unknown <chiscazo.paco@gmail.com>
2018-01-31 19:08:52 +0100

    Finalizado clasecomercio.

--- FRMCode/NetBeansProjects/ProgramaComercio/src/programacomercio/Metodos.java
+++ FRMCode/NetBeansProjects/ProgramaComercio/src/programacomercio/Metodos.java
@@ -2,29 +2,47 @@ package programacomercio;
 
 public class Metodos {
     public void impfact(Ventas[]v,Productos[]pro,Prezos[]pre){
+        System.out.println("Factura final");
         String aux;
-        int c=0;
+        float c=0;
         for(int i=0;i<3;i++){
-            System.out.println(v[i].nv);
+            System.out.print(v[i].nv+" --- ");
             aux=v[i].getRprod();
             for(int x=0;x<3;x++){
                 if(aux.equals(pro[x].getRprod())){
-                    System.out.println(pro[x].getNome());
-                    aux=pre[x].getRprez();
-                    System.out.println(aux);
-//                    for(int y=0;y<3;y++){
-//                        if(aux.equals(pre[y].getRprez())){
-//                            System.out.println(pre[y].getPrez());
-//                        }
-//                    }
-                }else{
-                    System.out.println(aux+pro[x].getRprod());
+                    System.out.print(pro[x].getNome()+" --- ");
+                    aux=pro[x].getRprez();
+                    for(int y=0;y<3;y++){
+                        if(aux.equals(pre[y].getRprez())){
+                            System.out.print(pre[y].getPrez()+"€/ud. subtotal:"+pre[y].getPrez()*v[i].getCant()+"\n");
+                            c=c+pre[y].getPrez()*v[i].getCant();
+                        }
+                    }
                 }
             }
             
         }
+        System.out.println("Total: "+c);
     }
-    public void searchelem(){
-
+    public void searchelem(Ventas[]v,Productos[]pro,Prezos[]pre, String m){
+        String aux=m;
+        for(int i=0;i<3;i++){
+            if(aux.equals(v[i].getNv())){
+            System.out.println("Numero de venta do producto: "+v[i].nv);
+            aux=v[i].getRprod();
+            for(int x=0;x<3;x++){
+                if(aux.equals(pro[x].getRprod())){
+                    System.out.println("Nome do Producto: "+pro[x].getNome());
+                    aux=pro[x].getRprez();
+                    for(int y=0;y<3;y++){
+                        if(aux.equals(pre[y].getRprez())){
+                            System.out.println("Prezo unitario do producto: "+pre[y].getPrez()+"€");
+                           
+                        }
+                    }
+                }
+            }
+        }
+        }
     }
 }
--- FRMCode/NetBeansProjects/ProgramaComercio/src/programacomercio/ProgramaComercio.java
+++ FRMCode/NetBeansProjects/ProgramaComercio/src/programacomercio/ProgramaComercio.java
@@ -5,13 +5,13 @@ public class ProgramaComercio {
 
     public static void main(String[] args) {
         Ventas[]v1={new Ventas("1","rf1",7),new Ventas("2","rf2",9),new Ventas("3","rf3",4)};
-        Productos[]p1={new Productos("1","rf1","7"),new Productos("2","rf2","9"),new Productos("3","rf3","4")};
+        Productos[]p1={new Productos("rf1","Uvas","rp1"),new Productos("rf2","Coles","rp2"),new Productos("rf3","fresas","rp3")};
         Prezos[]pr1={new Prezos("rp1",2),new Prezos("rp2",5),new Prezos("rp3",4)};
         Metodos m1=new Metodos();
         m1.impfact(v1, p1, pr1);
         
-        
-        //String memo=Entradaspred.pedirString("Introduce numero de venta");
+        String memo=Entradaspred.pedirString("Introduce numero de venta");
+        m1.searchelem(v1, p1, pr1, memo);
         
     }
     

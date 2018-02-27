3acbc72d341b72fabb30f0a4142c600feac9fd7a
Femio23 <chiscazo.paco@gmail.com>
2018-02-27 10:11:36 +0100

    Avances ProxectoAlarma.

--- FRMCode/NetBeansProjects/ProxectoAlarma/src/proxectoalarma/ProxectoAlarma.java
+++ FRMCode/NetBeansProjects/ProxectoAlarma/src/proxectoalarma/ProxectoAlarma.java
@@ -5,20 +5,97 @@
  */
 package proxectoalarma;
 
+import java.awt.Toolkit;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Timer;
+import java.util.TimerTask;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author femio23
  */
 public class ProxectoAlarma {
+    Timer timer;
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Timer t1;
+        
         PanelAlarma pa1=new PanelAlarma();
+
+        
+        new ProxectoAlarma();
+        
+        
+        
+        
+        
     }
+ 
+
+        public ProxectoAlarma() {
+            ProgramaAlarma pa1=new ProgramaAlarma();
+            Timer timer=new Timer();
+            timer.schedule(new ProgramaAlarma(),0,1000);
+        }
     
+    
+    
+    
+    public class ProgramaAlarma extends TimerTask{
+        Date hora,horad;
+
+        public Date getHora() {
+            return hora;
+        }
+
+        public void setHora(Date hora) {
+            this.hora = hora;
+        }
+
+        public Date getHorad() {
+            return horad;
+        }
+
+        public void setHorad(Date horad) {
+            this.horad = horad;
+        }
+
+        public ProgramaAlarma(Date horad) {
+            this.horad = horad;
+        }
+
+        public ProgramaAlarma() {
+        }
+
+
+        @Override
+        public void run(){
+            SimpleDateFormat formato=new SimpleDateFormat("hh:mm:ss");
+            hora=new Date();
+            try {
+                Date a=formato.parse("Thu Jan 01 10:07:30 CET 1970");
+                horad=formato.format(a);
+            } catch (ParseException ex) {
+                Logger.getLogger(ProxectoAlarma.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+            if(hora==horad){
+                System.out.println("Despertaaaa.");
+                Toolkit son=Toolkit.getDefaultToolkit();
+                son.beep();
+                timer.cancel();
+                timer.purge();
+            }else{
+                System.out.println("Son as "+formato.format(hora)+", toca ás "+horad+".");
+            }
+        }
+    }
 }
+

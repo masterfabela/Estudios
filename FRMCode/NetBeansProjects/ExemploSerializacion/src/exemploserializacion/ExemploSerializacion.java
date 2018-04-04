2a03cab473884751015b37e2f20bdcdb82055296
Femio23 <chiscazo.paco@gmail.com>
2018-04-04 10:08:39 +0200

    Avances serialización.

--- FRMCode/NetBeansProjects/ExemploSerializacion/src/exemploserializacion/ExemploSerializacion.java
+++ FRMCode/NetBeansProjects/ExemploSerializacion/src/exemploserializacion/ExemploSerializacion.java
@@ -1,21 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package exemploserializacion;
 
-/**
- *
- * @author femio23
- */
+package exemploserializacion;
+import java.util.ArrayList;
 public class ExemploSerializacion {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Test t1=new Test();
+        t1.engadir("Obj1");
     }
     
 }
--- FRMCode/NetBeansProjects/ExemploSerializacion/src/exemploserializacion/Test.java
+++ FRMCode/NetBeansProjects/ExemploSerializacion/src/exemploserializacion/Test.java
@@ -5,10 +5,20 @@
  */
 package exemploserializacion;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+
 /**
  *
  * @author femio23
  */
-public class Test {
+public class Test implements Serializable {
+    ArrayList obxetos=new ArrayList();
+    public void engadir(String n){
+        obxetos.add(n);
+    }
+    public ArrayList getObxetos(){
+        return obxetos;
+    }
     
 }

5ad32b89718f5047693f729c1b818dd6bb68784f
Femio23 <chiscazo.paco@gmail.com>
2018-05-22 16:19:44 +0200

    Avances SQL java

---
+++ FRMCode/NetBeansProjects/ProxectoSQL/src/proxectosql/Metodos.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proxectosql;
+
+import java.sql.Statement;
+
+/**
+ *
+ * @author femio23
+ */
+public class Metodos implements Statement{
+    
+}

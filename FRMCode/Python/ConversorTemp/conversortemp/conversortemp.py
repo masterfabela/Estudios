23afa2e96feeb1899f335c37025eb2266cfe5dad
Francisco Romay Méndez <chiscazo.paco@gmail.com>
2018-10-08 14:14:59 +0200

    Avances con python.

--- FRMCode/Python/ConversorTemp/conversortemp/conversortemp.py
+++ FRMCode/Python/ConversorTemp/conversortemp/conversortemp.py
@@ -10,14 +10,42 @@ class Conversor:
         b = Gtk.Builder()
         b.add_from_file('Conversor.glade')
         self.venConversor = b.get_object("venConversor")
+        self.aviso = b.get_object("lblaviso")
+        self.resultado = b.get_object("lblresul")
+        self.entdato = b.get_object("txtTemp1")
+        self.cmbuno = b.get_object("cmbEscala1")
+        self.cmbdos = b.get_object("cmbEscala2")
+
         dic = {
-            'on_venConversor_destroy': self.salir
+            'on_venConversor_destroy': self.salir,
+            'on_btncalcular_clicked' :self.calcula
                }
         b.connect_signals(dic)
         self.venConversor.show()
-    def salir(self,widget, data = None):
+
+    def salir(self, widget, data = None):
         Gtk.main_quit()
-    
+
+    def celtofah(self):
+        temp = self.entdato.get_text()
+        fah = 1.4*float(temp) + 32.0
+        var= fah
+        return (var)
+
+    def calcula(self, widget):
+        self.select1 = self.cmbuno.get_active_text()
+        self.select2 = self.cmbdos.get_active_text()
+
+
+        if self.select1 == "Celsius" and self.select2 == "Farenheit":
+            self.resultado.set_text(self.celtofah())
+        if self.select1 == "Kelvin":
+            self.resultado.set_text(self.select2)
+        if self.select1 == "Farenheit":
+            self.resultado.set_text(self.select2)
+
+
+
 if __name__ == '__main__':
     main = Conversor()
     Gtk.main()

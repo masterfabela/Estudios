38db6c94d344d8b76481ba1e745c783c3e67d7e3
Francisco Romay Méndez <chiscazo.paco@gmail.com>
2018-10-08 14:32:48 +0200

    Avances no conversor.

--- FRMCode/Python/ConversorTemp/conversortemp/conversortemp.py
+++ FRMCode/Python/ConversorTemp/conversortemp/conversortemp.py
@@ -15,6 +15,7 @@ class Conversor:
         self.entdato = b.get_object("txtTemp1")
         self.cmbuno = b.get_object("cmbEscala1")
         self.cmbdos = b.get_object("cmbEscala2")
+        self.var=0
 
         dic = {
             'on_venConversor_destroy': self.salir,
@@ -30,18 +31,46 @@ class Conversor:
         temp = self.entdato.get_text()
         fah = 1.4*float(temp) + 32.0
         var= fah
+        var=str(var)
         return (var)
+    def celtokel(self):
+
+        return ()
+
+    def fahtokel(self):
+
+        return ()
+    def fahtocel(self):
+
+        return ()
+    def keltocel(self):
+
+        return ()
+    def keltofah(self):
+        #despois facer esto con radiobutton
+        return ()
 
     def calcula(self, widget):
         self.select1 = self.cmbuno.get_active_text()
         self.select2 = self.cmbdos.get_active_text()
 
-
         if self.select1 == "Celsius" and self.select2 == "Farenheit":
-            self.resultado.set_text(self.celtofah())
-        if self.select1 == "Kelvin":
+            self.var = self.celtofah()
+            self.resultado.set_text(self.var)
+        if self.select1 == "Celsius" and self.select2 == "Kelvin":
+            self.celtokel()
             self.resultado.set_text(self.select2)
-        if self.select1 == "Farenheit":
+        if self.select1 == "Kelvin"and self.select2 == "Farenheit":
+            self.keltofah()
+            self.resultado.set_text(self.select2)
+        if self.select1 == "Kelvin" and self.select2 == "Celsius":
+            self.keltocel()
+            self.resultado.set_text(self.select2)
+        if self.select1 == "Farenheit" and self.select2 == "Celsius":
+            self.fahtocel()
+            self.resultado.set_text(self.select2)
+        if self.select1 == "Farenheit" and self.select2 == "Kelvin":
+            self.fahtokel()
             self.resultado.set_text(self.select2)
 
 

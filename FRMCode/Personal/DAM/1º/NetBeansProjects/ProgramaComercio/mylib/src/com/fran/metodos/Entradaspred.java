781d1cb2f1c8e36e581bdabad7e585d39afc9fab
Francisco Romay Méndez <chiscazo.paco@gmail.com>
2018-10-11 10:24:31 +0200

    Cambio de orde 2.

---
+++ FRMCode/Personal/DAM/1º/NetBeansProjects/ProgramaComercio/mylib/src/com/fran/metodos/Entradaspred.java
@@ -0,0 +1,10 @@
+package com.fran.metodos;
+
+import javax.swing.JOptionPane;
+
+public class Entradaspred {
+    public static String pedirString(String s){
+    String mem=JOptionPane.showInputDialog(s);
+    return mem;
+    }
+}

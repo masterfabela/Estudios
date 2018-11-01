beea78b75b1bfc38338915ab53255767aba5b911
Femio <chiscazo.paco@gmail.com>
2018-11-01 11:19:35 +0100

    Cambios.

---
+++ FRMCode/AAD/Exercicio4/src/Obx/Autores.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Obx;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author a18franciscorm
+ */
+public class Autores implements Serializable {
+    private int dniautor;
+    private String nome;
+    private String nacionalidade;
+
+    public Autores() {
+    }
+
+    public Autores( int dniautor, String nome, String nacionalidade) {
+        this.dniautor = dniautor;
+        this.nome = nome;
+        this.nacionalidade = nacionalidade;
+    }
+
+    public int getDniautor() {
+        return dniautor;
+    }
+
+    public void setDniautor(int dniautor) {
+        this.dniautor = dniautor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+
+    
+}
---
+++ FRMCode/AAD/Exercicio4/src/exercicio4/Exercicio4.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicio4;
+import Obx.*;
+import java.util.Scanner;
+import org.hibernate.*;
+
+/**
+ *
+ * @author a18franciscorm
+ */
+public class Exercicio4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        MetodosSQL msql=new MetodosSQL();
+        Metodos m1= new Metodos();
+        msql.creaTaboas();
+        SessionFactory sf= HibernateUtil.getSessionFactory();
+        Session sesion =HibernateUtil.getSession();
+        sf.openSession();
+        Transaction tr=sesion.beginTransaction();
+        Scanner sc=new Scanner(System.in);
+        byte opcion=0;
+        while(opcion!=6){
+            
+            switch(opcion){
+                case 1:;
+                break;
+                default:opcion=m1.menuPrincipal();
+                break;
+            }
+        }
+        Libros l1 = new Libros(4563,"Lolita",29.3f);
+        sesion.save(l1);
+        Autores a1= new Autores(77416900,"Francisco Romay","Española");
+        sesion.save(a1);
+        Telefonos t1=new Telefonos(77416900,986744755);
+        sesion.save(t1);
+        tr.commit();
+        sesion.close();
+        sf.close();
+        /*
+        La relación entre Autores  y Libros  es de  uno a varios:un autor puede escribir
+        varios libros y un libro  solo puede tener un autor, en los pojos pondremos la 
+        relación bidireccional. La relación entre Autores y teléfonos es de uno a uno,
+        un autor solo puede tener un tf.Y un tf solo puede pertenecer a un autor.
+        MENÚ
+        2- Inserción de nuevas filas.
+            a. Inserción autor.
+            b. Inserción libro(tiene que existir el autor).
+            c. Insercción teléfono.
+        3- Borrado de filas.
+            a. Borrado libro(El autor sigue existiendo).
+            b. Borrado autor(Desaparecen todos los libros).
+            c. Borrado teléfono
+        4-Modificaciones.
+            - Introduciendo el código de un libro modifique su precio.
+            -partiendo de un autor modificar el nº de tf.	
+        6- Consultas.
+            a. Introduciendo el título de un libro visualice sus datos  
+                y el nombre el autor ó autores con su tf.
+            b. Introduciendo el nombre de un autor visualice sus libros.
+            c. Visualización de todos los Libros de la tabla.
+            d.Visualización  de todos los autores con sus libros.
+        7-Fin.
+
+        */
+    }
+    
+}

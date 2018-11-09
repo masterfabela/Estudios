78d03a0e9ea9f42117815d30245e83889d5b6a97
Francisco Romay Méndez <chiscazo.paco@gmail.com>
2018-11-09 13:13:11 +0100

    Finalización do exercicio Saudo V2.

--- FRMCode/PMDM/SaudoPers_v2/app/src/main/java/com/example/a18franciscorm/saudopers_v2/Main2Activity.java
+++ FRMCode/PMDM/SaudoPers_v2/app/src/main/java/com/example/a18franciscorm/saudopers_v2/Main2Activity.java
@@ -1,7 +1,11 @@
 package com.example.a18franciscorm.saudopers_v2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -9,6 +13,20 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        TextView textosaida =(TextView)findViewById(R.id.textosaida);
+        Bundle b1=getIntent().getExtras();
+        Button botvol=(Button)findViewById(R.id.butvolv);
+        botvol.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i3= new Intent(Main2Activity.this,MainActivity.class);
+                startActivity(i3);
+            }
+        });
+        if (b1.getInt("idade")>=18)
+            textosaida.setText("Hola, "+b1.getString("nome")+",és maior de idade.");
+        else
+            textosaida.setText("Hola, "+b1.getString("nome")+",és menor de idade.");
     }
-    //Engadir os saudos distintos, o sexo e se é menor de idade.
 }
--- FRMCode/PMDM/SaudoPers_v2/app/src/main/java/com/example/a18franciscorm/saudopers_v2/MainActivity.java
+++ FRMCode/PMDM/SaudoPers_v2/app/src/main/java/com/example/a18franciscorm/saudopers_v2/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.a18franciscorm.saudopers_v2;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,13 +16,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button btn1=(Button)findViewById(R.id.btn1);
-        EditText etIdade=(EditText)findViewById(R.id.etIdade);
-        EditText etNome=(EditText)findViewById(R.id.etNome);
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                EditText etIdade=(EditText)findViewById(R.id.etIdade);
+                EditText etNome=(EditText)findViewById(R.id.etNome);
+                Bundle datos = new Bundle();
                 Intent i= new Intent (MainActivity.this,Main2Activity.class);
+                if (comprobaEdits(etIdade,etNome)){
+                    datos.putString("nome",etNome.getText().toString());
+                    datos.putInt("idade",Integer.parseInt(etIdade.getText().toString()));
+                    i.putExtras(datos);
+                    startActivity(i);}
             }
         });
+    }
+    public boolean comprobaEdits(EditText idade, EditText nome){
+        if (idade.getText().toString().isEmpty() || nome.getText().toString().isEmpty()) {
+            if (idade.getText().toString().isEmpty())
+                idade.setHintTextColor(Color.RED);
+            if (nome.getText().toString().isEmpty())
+                nome.setHintTextColor(Color.RED);
+            return false;
+        }
+        else return true;
     }
 }

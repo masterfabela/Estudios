1a731bdeccdf44c49aa09bda687b7ce22a0b9f9f
Francisco Romay Méndez <chiscazo.paco@gmail.com>
2018-12-10 13:40:56 +0100

    Creación da ventá.

--- FRMCode/DI/ProxectoRestaurante/proxectorestaurante/proxectorestaurante.py
+++ FRMCode/DI/ProxectoRestaurante/proxectorestaurante/proxectorestaurante.py
@@ -1,6 +1,21 @@
-# To change this license header, choose License Headers in Project Properties.
-# To change this template file, choose Tools | Templates
-# and open the template in the editor.
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+
+class Restaurante:
+    def __init__(self):
+        int_visual = Gtk.Builder()
+        int_visual.add_from_file("Restaurante.glade")
+        self.vent_principal = int_visual.get_object("vent_principal")
+        self.vent_principal.show()
+
+# O camareiro asigna todolos campos da base,
+# as facturas so poden ser accedidas polo camareiro que atendeu a mesa, momento no cal, levanta dita factura.
+# as liñas de factura son os capos de productos que se engaden
+#
 
 if __name__ == "__main__":
-    print("Hello World")
+    print("Lanzase a aplicación.")
+    main = Restaurante()
+    Gtk.main()
+

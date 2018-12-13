07b1f3e8708b6b63872a8705cc741ee4ca027c06
Francisco Romay Méndez <chiscazo.paco@gmail.com>
2018-12-13 08:43:24 +0100

    a.

--- FRMCode/PMDM/Planetas/app/src/main/java/com/example/a18franciscorm/planetas/MainActivity.java
+++ FRMCode/PMDM/Planetas/app/src/main/java/com/example/a18franciscorm/planetas/MainActivity.java
@@ -2,12 +2,41 @@ package com.example.a18franciscorm.planetas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView salida;
+    Spinner spiner;
+    Button boton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        salida=(TextView)findViewById(R.id.salida);
+        spiner =  (Spinner)findViewById(R.id.spinner);
+        boton= (Button)findViewById(R.id.boton);
+        spiner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                salida.setText(String.valueOf(position+1));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Ping");
+                salida.setText(spiner.getSelectedItem().toString());
+            }
+        });
+
     }
 }

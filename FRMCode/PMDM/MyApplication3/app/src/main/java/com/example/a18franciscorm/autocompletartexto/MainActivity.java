f26bcc0f7cb8236ff6438c5dca9dd1cdc81557ed
Francisco Romay Méndez <chiscazo.paco@gmail.com>
2019-01-11 14:21:24 +0100

    Avances.

--- FRMCode/PMDM/MyApplication3/app/src/main/java/com/example/a18franciscorm/autocompletartexto/MainActivity.java
+++ FRMCode/PMDM/MyApplication3/app/src/main/java/com/example/a18franciscorm/autocompletartexto/MainActivity.java
@@ -2,12 +2,32 @@ package com.example.a18franciscorm.autocompletartexto;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private String[] arrayNombres;
+    AutoCompleteTextView entrada;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        arrayNombres=getResources().getStringArray(R.array.nombres);
+        entrada=(AutoCompleteTextView)findViewById(R.id.entrada);
+        final ArrayAdapter adaptador= new ArrayAdapter(this,android.R.layout.simple_list_item_1,arrayNombres);
+        entrada.setAdapter(adaptador);
+        entrada.setThreshold(1);
+        entrada.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this,"Elixiches o elemento "+(position+1),Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }

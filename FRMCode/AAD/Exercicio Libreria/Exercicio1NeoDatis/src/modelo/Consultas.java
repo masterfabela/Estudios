9a97f0861cd96e620b8048b7c2f50f9debe9c33c
Francisco Romay Méndez <chiscazo.paco@gmail.com>
2019-01-14 11:17:07 +0100

    Avances.

--- FRMCode/AAD/Exercicio Libreria/Exercicio1NeoDatis/src/modelo/Consultas.java
+++ FRMCode/AAD/Exercicio Libreria/Exercicio1NeoDatis/src/modelo/Consultas.java
@@ -5,13 +5,19 @@
  */
 package modelo;
 
+import java.time.LocalDate;
 import java.util.Scanner;
 import org.neodatis.odb.ODB;
 import org.neodatis.odb.ODBFactory;
+import org.neodatis.odb.ObjectValues;
 import org.neodatis.odb.Objects;
+import org.neodatis.odb.Values;
 import org.neodatis.odb.core.query.IQuery;
+import org.neodatis.odb.core.query.criteria.And;
+import org.neodatis.odb.core.query.criteria.ICriterion;
 import org.neodatis.odb.core.query.criteria.Where;
 import org.neodatis.odb.impl.core.query.criteria.CriteriaQuery;
+import org.neodatis.odb.impl.core.query.values.ValuesCriteriaQuery;
 
 /**
  *
@@ -46,18 +52,23 @@ public class Consultas {
                 System.out.println(l1.toString());
             }
             odb.close();
-        }}
-    public void librosPublicadosIntervalo(){}
-    public void librosEspañois60(){
-         ODB odb=ODBFactory.openClient("localhost", 8000, "libreria");
-        IQuery query2=new CriteriaQuery(Autor.class,Where.equal("nacionalidad","española");
-        Objects<Autor> lista=odb.getObjects(query2);
+        }
+    }
+    public void librosPublicadosIntervalo(){
+        //Metodo non finalizado.
+        LocalDate fecha1=null,fecha2=null;
+        ODB odb=ODBFactory.openClient("localhost", 8000, "libreria");
+        Scanner sc=new Scanner(System.in);
+        System.out.println("");
+        ICriterion criterio= new And().add(Where.equal("nacionalidad","ESP")).add(Where.gt("fechaPublicacion",fecha1)).add(Where.ge("fechaPublicacion",fecha2));
+        CriteriaQuery query = new CriteriaQuery(Autor.class,criterio);
+        Objects<Autor> lista=odb.getObjects(query);
         if(lista.isEmpty()){
-            System.out.println("Non existen autores españois.");
+            System.out.println("Non existen autores españois e maiores de 60.");
             odb.close();
         }else{
             for(Autor l1:lista){
-                System.out.println(l1.getNombre()+", de "+" años:");
+                System.out.println(l1.getNombre()+", de "+l1.getEdad()+" años:");
                 if(l1.getLibros().isEmpty())
                     System.out.println("Non ten libros publicados.");
                 else{
@@ -69,7 +80,36 @@ public class Consultas {
             odb.close();
         }
     }
-    public void cantidadNacional(){}
+    public void librosEspañois60(){
+        ODB odb=ODBFactory.openClient("localhost", 8000, "libreria");
+        ICriterion criterio= new And().add(Where.equal("nacionalidad","ESP")).add(Where.gt("edad",18));
+        CriteriaQuery query = new CriteriaQuery(Autor.class,criterio);
+        Objects<Autor> lista=odb.getObjects(query);
+        if(lista.isEmpty()){
+            System.out.println("Non existen autores españois e maiores de 60.");
+            odb.close();
+        }else{
+            for(Autor l1:lista){
+                System.out.println(l1.getNombre()+", de "+l1.getEdad()+" años:");
+                if(l1.getLibros().isEmpty())
+                    System.out.println("Non ten libros publicados.");
+                else{
+                    for(Libro l2:l1.getLibros()){
+                        l2.getTitulo();
+                    }
+                }
+            }
+            odb.close();
+        }
+    }
+    public void cantidadNacional(){
+        ODB odb=ODBFactory.openClient("localhost", 8000, "libreria");
+        Values listaNacions= odb.getValues(new ValuesCriteriaQuery(Autor.class).field("nacionalidad").count("dni").groupBy("nacionalidad"));
+        while(listaNacions.hasNext()){
+            ObjectValues ov=(ObjectValues)listaNacions.next();
+            System.out.println("Pais: "+ov.getByAlias("nacionalidad")+", autores: "+ov.getByIndex(1));
+        }
+    }
     public void titulosAutor(){
         ODB odb=ODBFactory.openClient("localhost", 8000, "libreria");
         Scanner sc= new Scanner(System.in);

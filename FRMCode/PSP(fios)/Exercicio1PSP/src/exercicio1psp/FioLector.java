3599ee5c42560d4af1dab54a73b5bef99a60264c
Francisco Romay Méndez <chiscazo.paco@gmail.com>
2019-02-14 14:24:16 +0100

    Avances

--- FRMCode/PSP(fios)/Exercicio1PSP/src/exercicio1psp/FioLector.java
+++ FRMCode/PSP(fios)/Exercicio1PSP/src/exercicio1psp/FioLector.java
@@ -15,42 +15,34 @@ import java.util.LinkedList;
  * @author a18franciscorm
  */
 public class FioLector extends Thread {
-
     final private BufferedReader br;
     private int lonxitude = 0;
     private String iniciais = "";
     LinkedList<Linea> ll;
-    Comparator c=(Comparator<Linea>) (Linea o1, Linea o2) -> o1.getIniciais().compareTo(o2.getIniciais());
+    Escritor esc=new Escritor();
 
     @Override
     public void run() {
         try {
-            while (br.ready()) {
+            String cadea=br.readLine();
+            while (!cadea.equalsIgnoreCase(null)){
                 iniciais = "";
                 lonxitude = 0;
-                String cadea=br.readLine();
                 String[] fraccion = cadea.split(" ");
                 for (String d1 : fraccion) {
                     iniciais += d1.charAt(0);
                     lonxitude += d1.length();
                 }
-                gardado(cadea,iniciais,lonxitude);
+                Linea test=new Linea(iniciais,cadea,lonxitude);
+                esc.gardado(test,ll);
+                cadea=br.readLine();
             }
+            br.close();
         } catch (IOException ioe1) {
             ioe1.getMessage();
         }
+        
     }
-    public synchronized void gardado(String cadea,String iniciais,int lonxude){
-        for(int i=0;i<ll.size();i++){
-            if(ll.get(i).getCadea().equals(cadea)){
-               ll.get(i).setCantidade(ll.get(i).getCantidade()+1);
-           }else{
-               ll.add(new Linea(iniciais,cadea,lonxude,1));
-               ll.sort(c);
-           }
-        }
-        }
-    
     public FioLector(BufferedReader br, LinkedList ll) {
         this.br = br;
         this.ll = ll;

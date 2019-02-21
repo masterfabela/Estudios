16ac45bea5e9e2f88ed59fab22dcbf782977ba2b
Francisco Romay Méndez <chiscazo.paco@gmail.com>
2019-02-21 13:52:08 +0100

    Avances Proxecto Android.

--- FRMCode/PMDM/Proxecto/app/src/main/java/com/example/a18franciscorm/proxecto/ClaseDB.java
+++ FRMCode/PMDM/Proxecto/app/src/main/java/com/example/a18franciscorm/proxecto/ClaseDB.java
@@ -1,5 +1,6 @@
 package com.example.a18franciscorm.proxecto;
 
+import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -9,14 +10,20 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class ClaseDB extends SQLiteOpenHelper {
 
-    String sqlCreate = "CREATE TABLE Alimentos (codigo INTEGER PRIMARY KEY, nombre TEXT, tipo text)";
+    String sqlCreate = "CREATE TABLE Alimentos (codigo INTEGER PRIMARY KEY, nombre TEXT, tipo TEXT,azucares_simples TEXT, grasas_saturadas TEXT, sodio TEXT);";
+
+    public ClaseDB(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL(sqlCreate);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL("DROP TABLE IF EXISTS Alimentos;");
+        db.execSQL(sqlCreate);
     }
 }
--- FRMCode/PMDM/Proxecto/app/src/main/java/com/example/a18franciscorm/proxecto/MainActivity.java
+++ FRMCode/PMDM/Proxecto/app/src/main/java/com/example/a18franciscorm/proxecto/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.a18franciscorm.proxecto;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Integrar nunha bd 12 elementos da lista e realizar as consultas sobre el.
+        //o monitor solo vai na 22, ata que execute o comando máxico.
+        ClaseDB cdb= new ClaseDB(this,"BDAlimentos",null,1);
+        SQLiteDatabase sqldb=cdb.getReadableDatabase();
+        sqldb.execSQL("insert into Alimentos(codigo,nombre,tipo,azucares_simples,grasas_saturadas,sodio) values (03,'Arroz_con_leite','Lacteo','17','0.77','38')");
+        sqldb.close();
+        System.out.println("Insercion realizada");
     }
 }

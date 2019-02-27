e1c0947a9a915ffe77eb08a4b8eee1621e3c17da
Francisco Romay Méndez <chiscazo.paco@gmail.com>
2019-02-27 09:31:56 +0100

    Creados os intents da aplicación.

---
+++ FRMCode/PMDM/Proxecto/app/src/main/java/com/example/a18franciscorm/proxecto/ActivityXestion.java
@@ -0,0 +1,21 @@
+package com.example.a18franciscorm.proxecto;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityXestion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_xestion);
+        //Integrar nunha bd 12 elementos da lista e realizar as consultas sobre el.
+        //o monitor solo vai na 22, ata que execute o comando máxico.
+        ClaseDB cdb= new ClaseDB(this,"BDAlimentos",null,1);
+        SQLiteDatabase sqldb=cdb.getReadableDatabase();
+        sqldb.execSQL("insert into Alimentos(codigo,nombre,tipo,azucares_simples,grasas_saturadas,sodio) values (03,'Arroz_con_leite','Lacteo','17','0.77','38')");
+        sqldb.close();
+        System.out.println("Insercion realizada");
+    }
+}

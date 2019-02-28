e465055296ec72a48dfc368629d71410e43dd687
Francisco Romay Méndez <chiscazo.paco@gmail.com>
2019-02-28 10:20:08 +0100

    Avances.

--- FRMCode/PMDM/Proxecto/app/src/main/java/com/example/a18franciscorm/proxecto/AdaptadorPersonalizadp.java
+++ FRMCode/PMDM/Proxecto/app/src/main/java/com/example/a18franciscorm/proxecto/AdaptadorPersonalizadp.java
@@ -32,17 +32,89 @@ public class AdaptadorPersonalizadp extends ArrayAdapter {
         LayoutInflater inflador=context.getLayoutInflater();
         View fila=inflador.inflate(R.layout.fila,null);
         TextView tipo=(TextView)fila.findViewById(R.id.tipo);
-        tipo.setText("Tipo: "+animais.get(position).getTipo().toString());
+        tipo.setText(animais.get(position).getTipo().toString().replace('_',' '));
         TextView nome=(TextView)fila.findViewById(R.id.nome);
-        nome.setText("Nome:"+animais.get(position).getNome().toString());
-        ImageView img1,img2,img3;
+        nome.setText(animais.get(position).getNome().toString().replace('_',' '));
+        ImageView img1,img2,img3,imgres;
         img1=(ImageView)fila.findViewById(R.id.azucares);
         img2=(ImageView)fila.findViewById(R.id.grasas);
         img3=(ImageView)fila.findViewById(R.id.sodio);
+        imgres=(ImageView)fila.findViewById(R.id.resultado);
         String stringTipo=animais.get(position).getTipo().toString();
+        float azucre = Float.parseFloat(animais.get(position).getAzucares());
+        float graxa = Float.parseFloat(animais.get(position).getGrasas());
+        float sodio = Float.parseFloat(animais.get(position).getSodio());
+        boolean azucreVerde,azucreLaranxa,azucreRojo,graxaVerde,graxaLaranxa,graxaRojo,sodioVerde,sodioLaranxa,sodioRojo;
+        azucreVerde=azucre<5f;
+        azucreLaranxa=azucre<10f;
+        azucreRojo=azucre>=10f;
+        graxaVerde=graxa<3f;
+        graxaLaranxa=graxa<20f;
+        graxaRojo=graxa>=20f;
+        sodioVerde=sodio<120f;
+        sodioLaranxa=sodio<600f;
+        sodioRojo=sodio>=600f;
         if(stringTipo.equalsIgnoreCase("Froita")){
-            img1.setImageDrawable("");
+            img1.setImageResource(R.drawable.color_green_peque);
+            img2.setImageResource(R.drawable.color_green_peque);
+            img3.setImageResource(R.drawable.color_green_peque);
+        }else{
+
+            if(azucreVerde){
+                img1.setImageResource(R.drawable.color_green_peque);
+            }else{
+                if(azucreLaranxa){
+                    img1.setImageResource(R.drawable.color_orange_peque);
+                }else{
+                    if(azucreRojo){
+                        img1.setImageResource(R.drawable.color_red_peque);
+                    }
+                }
+            }
+            if(graxaVerde){
+                img2.setImageResource(R.drawable.color_green_peque);
+            }else{
+                if(graxaLaranxa){
+                    img2.setImageResource(R.drawable.color_orange_peque);
+                }else{
+                    if(graxaRojo){
+                        img2.setImageResource(R.drawable.color_red_peque);
+                    }
+                }
+            }
+            if(sodioVerde){
+                img3.setImageResource(R.drawable.color_green_peque);
+            }else{
+                if(sodioLaranxa){
+                    img3.setImageResource(R.drawable.color_orange_peque);
+                }else{
+                    if(sodioRojo){
+                        img3.setImageResource(R.drawable.color_red_peque);
+                    }
+                }
+            }
         }
+        if(azucreVerde && graxaVerde && sodioVerde){
+            imgres.setImageResource(R.drawable.color_green_grande);
+        }else{
+            if((azucreLaranxa && graxaLaranxa && sodioLaranxa)
+                    ||(azucreVerde&&graxaVerde&&sodioLaranxa)||(azucreVerde&&graxaLaranxa&&sodioVerde)||(azucreLaranxa&&graxaVerde&&sodioVerde)
+                    ||(azucreVerde&&graxaLaranxa&&sodioLaranxa)||(azucreLaranxa&&graxaLaranxa&&sodioVerde)||(azucreLaranxa&&graxaVerde&&sodioLaranxa)
+                    ||(azucreVerde&&graxaVerde&&sodioRojo)||(azucreVerde&&graxaRojo&&sodioVerde)||(azucreRojo&&graxaVerde&&sodioVerde)){
+                imgres.setImageResource(R.drawable.color_orange_grande);
+            }else{
+                if((azucreRojo && graxaRojo && sodioRojo)
+                        ||(azucreVerde && graxaRojo && sodioRojo)||(azucreRojo && graxaVerde && sodioRojo)||(azucreRojo && graxaRojo && sodioVerde)
+                        ||(azucreLaranxa&&graxaLaranxa&&sodioRojo)||(azucreLaranxa&&graxaRojo&&sodioLaranxa)||(azucreRojo&&graxaLaranxa&&sodioLaranxa)
+                        ||(azucreLaranxa&&graxaRojo&&sodioRojo)||(azucreRojo&&graxaLaranxa&&sodioRojo)||(azucreRojo && graxaRojo && sodioLaranxa)
+                        ||(azucreVerde && graxaLaranxa && sodioRojo)||(azucreLaranxa&&graxaRojo&&sodioVerde)||(azucreRojo&&graxaVerde&&sodioLaranxa)){
+                    imgres.setImageResource(R.drawable.color_red_grande);
+                }else{
+                    imgres.setImageResource(R.drawable.color_orange_grande);
+                }
+            }
+        }
+
         return fila;
     }
 }

7405e84b46d534bc05a852375adfa6b018f79042
Francisco Romay Méndez <chiscazo.paco@gmail.com>
2019-03-01 10:42:18 +0100

    Avances Exercicio Exposición.

---
+++ FRMCode/PSP(fios)/PresentacionPSPServer/src/presentacionpspserver/Servidor.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package presentacionpspserver;
+
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
+/**
+ *
+ * @author a18franciscorm
+ */
+public class Servidor implements RMICalcInterface {
+
+    public static void main(String[] args) {
+        Registry reg = null;
+        if (System.getSecurityManager() == null) {
+            SecurityManager sm = new SecurityManager();
+            System.setSecurityManager(sm);
+        }
+        try {
+            reg = LocateRegistry.createRegistry(5555);
+            System.out.println("Registro creado.");
+        } catch (Exception e) {
+            System.out.println("Error en la creación del registro:");
+            e.printStackTrace();
+        }
+        Servidor serverObject = new Servidor();
+        try {
+            reg.rebind("Calculadora", (RMICalcInterface) UnicastRemoteObject.exportObject(serverObject, 0));
+            System.out.println("Registro inscrito.");
+        } catch (Exception e) {
+            System.out.println("Error en la inscripción del registro:");
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public int suma(int a, int b) throws RemoteException {
+        int resultado = a + b;
+        System.out.println("El resultado de la suma es: " + resultado);
+        return resultado;
+    }
+
+    @Override
+    public int resta(int a, int b) throws RemoteException {
+        int resultado = a - b;
+        System.out.println("El resultado de la resta es: " + resultado);
+        return resultado;
+    }
+
+    @Override
+    public int multip(int a, int b) throws RemoteException {
+        int resultado = a * b;
+        System.out.println("El resultado de la multiplicación es: " + resultado);
+        return resultado;
+    }
+
+    @Override
+    public int div(int a, int b) throws RemoteException {
+        int resultado = a / b;
+        System.out.println("El resultado de la división es: " + resultado);
+        return resultado;
+    }
+}
